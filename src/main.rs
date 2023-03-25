e24787d44d25957c018df43fbbe32ad353fccf0f
Eric Rowles <ericjacobr1999@gmail.com>
2023-03-25 16:14:15 -0600

    reactivity testing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
+#![allow(non_snake_case)]
+
 use sycamore::prelude::*;
 
+
+fn MainComponent<G: Html>(cx: Scope) -> View<G> {
+    let state = create_signal(cx, 0);
+    view! { cx,
+        div {
+            h1 { "Hello, world!" }
+            button(on:click=|_| {
+                state.set(*state.get() + 1);
+            }) {
+                "Click me!"
+            }
+            p {
+                "Current count: " (state.get())
+            }
+        }
+    }
+}
+
 fn main() {
-    sycamore::render(|cx| view! { cx,
-        p { "Hello, World!" }
-    });
+    sycamore::render(|cx| 
+        MainComponent(cx)
+    );
 }
